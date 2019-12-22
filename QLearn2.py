751dcd9ab4b0709321cddfc603e4090d315595bb
Chirag Vaswani <chirag9-vaswani@hotmail.es>
2019-12-22 08:34:55 +0000

    Pose investigations

--- QLearn2.py
+++ QLearn2.py
@@ -4,6 +4,7 @@ from random import randint
 
 import asyncio
 import cozmo.util
+from cozmo import world
 import time
 import cozmo
 from cozmo.util import distance_mm, speed_mmps
@@ -24,7 +25,7 @@ _clad_enum = _clad_to_engine_cozmo.ExecutableBehaviorType
 #Reference to Cozmo throughout the whole program
 robot = cozmo.robot.Robot
 
-#Where 1 = far dist, 2 = close dist
+#Where 0 = far dist, 1 = close dist
 states = [0,1]
 #Where 1 = forward, 2 = backward, 3 = greet, 4 = idle
 actions = [0,1,2,3]
@@ -34,6 +35,7 @@ rewards = [[-1.1,-2.1,-1.5,-2],[-2.1,-0.1,0.5,0]]
 gamma = 0.8
 Q = [[0,0,0,0],[0,0,0,0]]
 initState = 0
+nextActionIndex = 0
 
 def availActions(state):
     currentStateRow = rewards[state][:]
@@ -44,65 +46,145 @@ allActs = availActions(initState)
 
 def robotMovement(actionNum,robot:cozmo.robot.Robot):
     if(actionNum == 0):
-        print("HEELLO")
         robot.drive_straight(distance_mm(-250),speed_mmps(50)).wait_for_completed()
     elif(actionNum == 1):
-        print("HEEEEEELO2")
         robot.drive_straight(distance_mm(250),speed_mmps(50)).wait_for_completed()
     elif(actionNum == 2):
-        print("Konne")
         robot.say_text("Hello how are you doing today?").wait_for_completed()
 
-def nextAction(allActs,robot: cozmo.robot.Robot):
+def nextAction(robot: cozmo.robot.Robot):
     nextActRand = randint(0,3)
     nextAct = allActs[nextActRand]
     indexOfNextAct = allActs.index(nextAct)
     robotMovement(indexOfNextAct,robot)
-    return indexOfNextAct
-
-print(robot)
-nextAct = nextAction(allActs,robot)
+    global nextActionIndex
+    nextActionIndex = indexOfNextAct
 
 #Updating Q values
 def update(currentState,action,gamma):
-    print("currentstate in the method update")
-    print(currentState)
-    print(np.max(rewards[currentState][:]))
     Q[currentState][action] = round(rewards[currentState][action] + gamma * np.max(rewards[currentState][:]),2)
-    return currentState
 
-update(initState,nextAct,gamma)
+def findCurrentState(robot: cozmo.robot.Robot):
+    cozmo.run_program(moveRobotHead)
+    face = searchForFace(robot)
+    return face
 
+def searchForFace(robot: cozmo.robot.Robot):
+    face = None
+    while True:
+        if face and face.is_visible:
+            for face in robot.world.visible_faces:
+                if face.pose.position.x < float(500):
+                    currentState = 1
+                else:
+                    currentState = 0
+                return currentState
+        try:
+            face = robot.world.wait_for_observed_face(timeout=10)
+        except asyncio.TimeoutError:
+            print("Face not found")
+            return
+    # face = None
+    # try:
+    #     face = robot.world.wait_for_observed_face(timeout=10)
+    # except asyncio.TimeoutError:
+    #     robot.say_text("Sorry face not found")
+    # if face and face.is_visible:
+    #      for face in robot.world.visible_faces:
+    #          return face
+
+def moveRobotHead(robot:cozmo.robot.Robot):
+    robot.move_lift(-3)
+    robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE).wait_for_completed()
+
+def trainCozmo():
 #Training the model
-for i in range (50):
-    currentStateRand = randint(0,1)
-    action = nextAction(actions,robot)
-    eval = update(currentStateRand, action, gamma)
-    print(Q)
+    for i in range (10):
+        ##FINDS THE DISTANCE WITH THE HUMAN USING POSE
+        currentState = findCurrentState(robot)
+        #currentStateRand = randint(0,1)
+        cozmo.run_program(nextAction)
+        update(currentState, nextActionIndex, gamma)
 
+trainCozmo()
+#
+# if __name__=='__main__':
+#     cozmoThread = Process(target=trainCozmo)
+#     cozmoThread.start()
+#     listenerThread = Process(target=hcscenario3,args=(robot))
+#     listenerThread.start()
+#     cozmoThread.join()
+#     listenerThread.join()
 # Testing the model this time, commenting for robot interfacing
 
-def finalTest(robot:cozmo.robot.Robot):
-    sum = 0
-    initState = 0
-    for i in range (15):
-        print("This is the testing stage " + str(i))
-        bestVal= np.max(Q[initState][:])
-        sum+=bestVal
-        nextStep = Q[initState].index(bestVal)
-        robotMovement(nextStep,robot)
-        if(nextStep == 0):
-            nextStep = 1
-        elif(nextStep == 1):
-            nextStep = 0
-        elif(nextStep!= 1 or nextStep!=0):
-            nextStep = initState
-        initState = nextStep
+# def finalTest(robot:cozmo.robot.Robot):
+#     sum = 0
+#     initState = 0
+#     for i in range (15):
+#         print("This is the testing stage " + str(i))
+#         bestVal= np.max(Q[initState][:])
+#         sum+=bestVal
+#         nextStep = Q[initState].index(bestVal)
+#         robotMovement(nextStep,robot)
+#         if(nextStep == 0):
+#             nextStep = 1
+#         elif(nextStep == 1):
+#             nextStep = 0
+#         elif(nextStep!= 1 or nextStep!=0):
+#             nextStep = initState
+#         initState = nextStep
 
 print("THIS IS THE FINAL RESULT")
 print(Q)
-print(sum)
-robot.say_text("Hello how are you doing today mate").wait_for_completed()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def update(state,action):
+#     print("Before")
+#     print(rewards)
+#     print(rewards[state][action])
+#     rewards[state][action] = rewards[state][action] + exploreVExploit * (reward + gamma * np.max(rewards[state][:])) - rewards[state][action]
+#     print("After")
+#     print(rewards)
+
+
+
+
+#exploration v exploitation
+# gamma = 0.5
+# learnR = 0.5
+# epochs = 5
+
+
 
 
 
